8d58fadfda70b7b0f5561abc358c5bb27cdd05b1
Aman Juneja <71624646+amankn@users.noreply.github.com>
2021-10-27 11:02:50 +0000

    Implemented if and else statement.

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
+
+#Implementing if and else to replace ticket counter for users
+
+if height > 120:
+  print("You can ride the rollercoaster! ")
+else:
+  print("Sorry, you have to grow taller before you can ride.")
