28cd8d77b39948f00d2981c378d592b85147fe60
Aman Juneja <71624646+amankn@users.noreply.github.com>
2021-10-27 11:21:23 +0000

    implementing if/elif/else conditional statements

--- main.py
+++ main.py
@@ -3,7 +3,14 @@ height = int(input("What is your height in cm? "))
 
 #Implementing if and else to replace ticket counter for users
 
-if height > 120:
+if height >= 120:
   print("You can ride the rollercoaster! ")
+  age = int(input("What is your age "))
+  if age <= 12:
+    print("Please pay $5")
+  elif age <=18:
+    print("Please pay $7")
+  else:
+    print("Please pay $12.")
 else:
   print("Sorry, you have to grow taller before you can ride.")
